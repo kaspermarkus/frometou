9b0e3441ba5b693efbe879a45c908833c681bb67
NinjaAlienCow <sofus@markus.dk>
2013-04-17 17:38:07 +0200

    Just a little change visual change that i dint commint last time

--- web/CMS/functions/listDocs.php
+++ web/CMS/functions/listDocs.php
@@ -66,7 +66,7 @@ function ListDocs($did){
 	while ($row = mysql_fetch_array($result)) {
 		if ($did == $row['did']){
 			echo "<TR><TD><a class='selectedDoc' href='editDocs.php?did=".$row['did']."'> ".$row['ident']."</a>";
-			echo $_SESSION['lang'];
+			echo docFlags($row['did']);
 			echo "</TD></TR>";
 		} else {
 			echo "<TR><TD><a href='editDocs.php?did=".$row['did']."'> ".$row['ident']."</a> ";
