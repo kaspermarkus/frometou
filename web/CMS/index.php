e75316d064b17fdc3dcd6d1eafe694b9f137a074
Kasper Markus <kasper2@markus.dk>
2013-04-23 14:36:14 +0200

    Fisket fejlbeskeder, fjernet siteInfo.php fra repository og tilfoejet siteInfo.php i stedet

--- web/CMS/index.php
+++ web/CMS/index.php
@@ -1,8 +1,7 @@
 <?php 
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
 require_once("functions/authorize.php");
 ?>
+
 <html>
 <head>
 <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
